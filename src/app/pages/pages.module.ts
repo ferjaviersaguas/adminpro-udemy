c055396408256b59fcb72571e84c22aced6dcaab
Fernando Javier Saguas <fesaguas@everis.com>
2019-04-29 21:13:46 -0300

    Agregamos y modificamos modulos

---
+++ src/app/pages/pages.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+
+//RUTAS
+import { PAGES_ROUTES } from './pages.routes';
+
+//MODULOS
+import { SharedModule } from '../shared/shared.module';
+
+//COMPONENTES
+import { PagesComponent } from './pages.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ProgressComponent } from './progress/progress.component';
+import { Graficas1Component } from './graficas1/graficas1.component';
+
+
+
+@NgModule({
+    declarations:[
+        PagesComponent,
+        DashboardComponent,
+        ProgressComponent,
+        Graficas1Component
+    ],
+    exports: [
+        DashboardComponent,
+        ProgressComponent,
+        Graficas1Component
+    ],
+    imports: [
+        PAGES_ROUTES,
+        SharedModule
+    ]
+})
+export class PagesModule{}
